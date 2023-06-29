c0b11b2542676b6d2ac6bfce5e5ed1d33ee47e63
Obarajames <omwoyoobara@gmail.com>
2023-06-29 20:12:43 +0300

    first commit

---
+++ social blog/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+  const [count, setCount] = React.useState(0)
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ social blog/src/README.md
@@ -0,0 +1,2 @@
+This is a to inform u my fellow team members create a u need to create a new file example is the App.jsx
+clone the repo first .
